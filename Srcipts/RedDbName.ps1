97b29d310b42e39ada053a52681e550ee2364730
menpengpeng <menpengpeng@163.com>
2017-11-24 16:05:27 +0800

    获取用户输入的数据库名并保存到记事本中

---
+++ Srcipts/RedDbName.ps1
@@ -0,0 +1,11 @@
+﻿####################################################
+## Author:menpengpeng
+## Datetime:2017-11-23
+####################################################
+Write-Host "-----请输入需要备份的数据库名称，输入0退出输入---"
+do {
+    $DBName = Read-Host 
+    if($DBName -ne 0){
+     $DBName >> D:\backup_sqldb\Srcipts\DBName.txt
+    }
+}while($DBName -ne 0)
